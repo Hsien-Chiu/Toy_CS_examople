0ffe5bab691d3e1aec7c2f31386ce3230885bd9d
Jyh-Miin Lin <jyhmiinlin@gmail.com>
2020-09-30 11:07:39 +0800

    Define a utils file. Inside utils.py define the class

---
+++ utils.py
@@ -0,0 +1,13 @@
+import scipy.sparse
+
+class dct_operator_2D:
+    def __init__(self, nx, ny):
+        """
+        Create a scipy.sparse.LinearOperator 
+        """
+        pass
+    def dot(self, vx):
+        """
+        Forward 2D dct
+        """
+        pass 
